1295a3b8bf7ce172570b9d675750b070d2252000
Róisín Grannell <r.grannell2@gmail.com>
2023-07-15 12:59:35 +0100

    added type signature

--- src/string/string.ts
+++ src/string/string.ts
@@ -165,7 +165,7 @@ type UnicodeCategoryData = {
   category: string;
 };
 
-function fixedFromCharCode (codePt) {
+function fixedFromCharCode (codePt: number) {
   if (codePt > 0xFFFF) {
       codePt -= 0x10000;
       return String.fromCharCode(0xD800 + (codePt >> 10), 0xDC00 + (codePt & 0x3FF));
